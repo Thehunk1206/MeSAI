bbd9d50cf449efb99114480aef5fff3b4347df62
thehunk1206 <khantauhid313@gmail.com>
2021-09-20 21:45:58 +0530

    VAE decoder module created using tf.keras subclassing API

--- model/encoder3d.py
+++ model/encoder3d.py
@@ -32,7 +32,7 @@ class Encoder3D(tf.keras.layers.Layer):
         super(Encoder3D, self).__init__(name=name,**kwargs)
 
         self._L2_reg_f = 1e-5
-        self.conv3d_1 = tf.keras.layers.Conv3D(
+        self.conv_1 = tf.keras.layers.Conv3D(
             filters=32,
             kernel_size= (3,3,3),
             strides=(1,1,1),
@@ -42,9 +42,9 @@ class Encoder3D(tf.keras.layers.Layer):
 
         self.spatial_dropout3d = tf.keras.layers.SpatialDropout3D(0.2)
 
-        self.conv3d_module_out_1 = Conv3d_module(filters=32)
+        self.conv_module_out_1 = Conv3d_module(filters=32)
 
-        self.conv3d_2_down = tf.keras.layers.Conv3D(
+        self.conv_2_down = tf.keras.layers.Conv3D(
             filters=32,
             kernel_size=32,
             strides=(2,2,2),
@@ -52,11 +52,11 @@ class Encoder3D(tf.keras.layers.Layer):
             kernel_regularizer=tf.keras.regularizers.L2(self._L2_reg_f)
         )
 
-        self.conv3d_module_2 = Conv3d_module(filters=64)
+        self.conv_module_2 = Conv3d_module(filters=64)
 
-        self.conv3d_module_out_2 = Conv3d_module(filters=64)
+        self.conv_module_out_2 = Conv3d_module(filters=64)
 
-        self.conv3d_3_down = tf.keras.layers.Conv3D(
+        self.conv_3_down = tf.keras.layers.Conv3D(
             filters=64,
             kernel_size=32,
             strides=(2,2,2),
@@ -64,10 +64,10 @@ class Encoder3D(tf.keras.layers.Layer):
             kernel_regularizer=tf.keras.regularizers.L2(self._L2_reg_f)
         )
 
-        self.conv3d_module_3 = Conv3d_module(filters=128)
-        self.conv3d_module_out_3 = Conv3d_module(filters=128)
+        self.conv_module_3 = Conv3d_module(filters=128)
+        self.conv_module_out_3 = Conv3d_module(filters=128)
 
-        self.conv3d_4_down = tf.keras.layers.Conv3D(
+        self.conv_4_down = tf.keras.layers.Conv3D(
             filters=128,
             kernel_size=32,
             strides=(2,2,2),
@@ -75,29 +75,29 @@ class Encoder3D(tf.keras.layers.Layer):
             kernel_regularizer=tf.keras.regularizers.L2(self._L2_reg_f)
         )
 
-        self.conv3d_module_4 = Conv3d_module(filters=256)
-        self.conv3d_module_5 = Conv3d_module(filters=256)
-        self.conv3d_module_out_5 = Conv3d_module(filters=256)
+        self.conv_module_4 = Conv3d_module(filters=256)
+        self.conv_module_5 = Conv3d_module(filters=256)
+        self.conv_module_out_5 = Conv3d_module(filters=256)
 
     def call(self, inputs: tf.Tensor, **kwargs) -> tuple:
         assert len(inputs.shape) == 5, f'Input tensor should be of 5D dim, given dim was {len(inputs.shape)}'
-        x = self.conv3d_1(inputs)
+        x = self.conv_1(inputs)
         x = self.spatial_dropout3d(x)
 
-        x_out1 = self.conv3d_module_out_1(x)
-        x = self.conv3d_2_down(x_out1)
+        x_out1 = self.conv_module_out_1(x)
+        x = self.conv_2_down(x_out1)
         
-        x = self.conv3d_module_2(x)
-        x_out2 = self.conv3d_module_out_2(x)
-        x = self.conv3d_3_down(x_out2)
+        x = self.conv_module_2(x)
+        x_out2 = self.conv_module_out_2(x)
+        x = self.conv_3_down(x_out2)
 
-        x = self.conv3d_module_3(x)
-        x_out3 = self.conv3d_module_out_3(x)
-        x = self.conv3d_4_down(x_out3)
+        x = self.conv_module_3(x)
+        x_out3 = self.conv_module_out_3(x)
+        x = self.conv_4_down(x_out3)
 
-        x = self.conv3d_module_4(x)
-        x = self.conv3d_module_5(x)
-        x_out4 = self.conv3d_module_out_5(x)
+        x = self.conv_module_4(x)
+        x = self.conv_module_5(x)
+        x_out4 = self.conv_module_out_5(x)
 
         return x_out1, x_out2, x_out3, x_out4 # out1-> c,h,w,d,32  out2-> c,h,w,d,64  out3-> c,h,w,d,128  out4-> c,h,w,d,256  
 
--- model/vae_decoder.py
+++ model/vae_decoder.py
@@ -25,7 +25,153 @@ SOFTWARE.
 '''
 
 import tensorflow as tf
+from sampling import Sampling
+from group_norm import GroupNormalization
+from conv3d_module import Conv3d_module
 
 class VAE_decoder(tf.keras.layers.Layer):
-    def __init__(self, trainable, name, dtype, dynamic, **kwargs):
-        super().__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
+    def __init__(self, name:str, feat_h:int, feat_w:int, feat_d:int, feat_c:int, **kwargs):
+        super(VAE_decoder, self).__init__(name=name,  **kwargs)
+
+        self.feat_h = feat_h
+        self.feat_w = feat_w
+        self.feat_d = feat_d
+        self.feat_c = feat_c
+        self._L2_reg_f = 1e-5
+
+        self.group_norm = GroupNormalization(groups=8)
+        self.relu1 = tf.keras.layers.ReLU()
+        self.conv1 = tf.keras.layers.Conv3D(
+            filters=16,
+            kernel_size=(3,3,3),
+            strides=2,
+            padding='same',
+            kernel_regularizer= tf.keras.regularizers.L2(l2=self._L2_reg_f)
+        )
+        self.flatten = tf.keras.layers.Flatten()
+
+        self.dense1 = tf.keras.layers.Dense(units=256, kernel_regularizer=tf.keras.regularizers.L2(l2=self._L2_reg_f))
+
+        self.dense_z_mean = tf.keras.layers.Dense(units=128)
+        self.dense_z_var  = tf.keras.layers.Dense(units=128)
+
+        self.sampling = Sampling(name='sampling_1')
+
+        self.dense2 = tf.keras.layers.Dense(units=(self.feat_h * self.feat_w * self.feat_d * (self.feat_c//32)))
+        self.relu2 = tf.keras.layers.ReLU()
+        self.reshape = tf.keras.layers.Reshape(target_shape=(self.feat_h, self.feat_w, self.feat_d, (self.feat_c//32)))
+
+        self.conv2 = tf.keras.layers.Conv3D(
+            filters=256,
+            kernel_size=(1,1,1),
+            strides=(1,1,1),
+            padding='same',
+            kernel_regularizer= tf.keras.regularizers.L2(l2=self._L2_reg_f)
+        )
+        self.upsample_conv2 = tf.keras.layers.UpSampling3D(size=(2,2,2))
+
+        self.conv3 = tf.keras.layers.Conv3D(
+            filters=128,
+            kernel_size=(1,1,1),
+            strides=(1,1,1),
+            padding='same',
+            kernel_regularizer= tf.keras.regularizers.L2(l2=self._L2_reg_f)
+        )
+        self.upsample_conv3 = tf.keras.layers.UpSampling3D(size=(2,2,2))
+        self.conv_module_1 = Conv3d_module(filters=128)
+
+        self.conv4 = tf.keras.layers.Conv3D(
+            filters=64,
+            kernel_size=(1,1,1),
+            strides=(1,1,1),
+            padding='same',
+            kernel_regularizer= tf.keras.regularizers.L2(l2=self._L2_reg_f)
+        )
+        self.upsample_conv4 = tf.keras.layers.UpSampling3D(size=(2,2,2))
+        self.conv_module_2 = Conv3d_module(filters=64)
+
+        self.conv5 = tf.keras.layers.Conv3D(
+            filters=32,
+            kernel_size=(1,1,1),
+            strides=(1,1,1),
+            padding='same',
+            kernel_regularizer= tf.keras.regularizers.L2(l2=self._L2_reg_f)
+        )
+        self.upsample_conv5 = tf.keras.layers.UpSampling3D(size=(2,2,2))
+        self.conv_module_3 = Conv3d_module(filters=32)
+
+        self.conv6_out = tf.keras.layers.Conv3D(
+            filters=3,
+            kernel_size=(1,1,1),
+            strides=(1,1,1),
+            padding='same'
+        )
+
+    def call(self, inputs:tf.Tensor, **kwargs)->tuple:
+        '''
+        inputs =  output from encoder3d's last layer 
+        '''
+        # GroupNorm + conv(256)
+        x           = self.group_norm(inputs)
+        x           = self.relu1(x)
+        x           = self.conv1(x)
+        
+        # flatten + z_mean + z_var + sampling
+        x_flatten   = self.flatten(x)
+        x           = self.dense1(x_flatten)
+        z_mean_out  = self.dense_z_mean(x)
+        z_var_out   = self.dense_z_var(x)
+        x           = self.sampling(z_mean=z_mean_out, z_log_var=z_var_out)
+
+        # dense + reshape to 3d volume
+        x           = self.dense2(x)
+        x           = self.relu2(x)
+        x           = self.reshape(x)
+
+        #conv2(256) + upsample -> (b,h,w,d,256)
+        x           = self.conv2(x)
+        x           = self.upsample_conv2(x)
+
+        #conv3(128) + upsample + Conv_module -> (b,h,w,d,128)
+        x           = self.conv3(x)
+        x           = self.upsample_conv3(x)
+        x           = self.conv_module_1(x)
+
+        #conv4(64) + upsample + Conv_module -> (b,h,w,d,64)
+        x           = self.conv4(x)
+        x           = self.upsample_conv4(x)
+        x           = self.conv_module_2(x)
+
+        #conv5(32) + upsample + Conv_module -> (b,h,w,d,32)
+        x           = self.conv5(x)
+        x           = self.upsample_conv5(x)
+        x           = self.conv_module_3(x)
+
+        #conv6(3) -> reconstructed output
+        x_vae_out = self.conv6_out(x)
+
+        return z_mean_out, z_var_out, x_vae_out
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'feat_h': self.feat_h,
+            'feat_w': self.feat_w,
+            'feat_d': self.feat_d,
+            'feat_c': self.feat_c
+        })
+        return config
+    
+    @classmethod
+    def from_config(cls, config):
+        return super().from_config(config)
+
+if __name__ == "__main__":
+    vae = VAE_decoder(name='enc_1')
+    # first call to the `vae` will create weights
+    y = vae(tf.ones(shape=(1, 5, 6, 4, 256)))
+
+    print("weights:", len(vae.weights))
+    print("trainable weights:", len(vae.trainable_weights))
+    print("config:", vae.get_config())
+    print(f"Y: {y[-1].shape}")
